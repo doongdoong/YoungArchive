2d19a29baadfd3f8b4b04a4a02e328f1aa5cc512
doongdoong <ksy940822@naver.com>
2019-08-11 23:08:05 +0900

    add member class

---
+++ src/main/java/member/Member.java
@@ -0,0 +1,47 @@
+package member;
+
+import java.time.LocalDateTime;
+
+public class Member {
+	private Long id;
+	private String email;
+	private String password;
+	private String name;
+	private LocalDateTime registerDateTime;
+	
+	public Member(String email, String password, String name, LocalDateTime registerDateTime) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.registerDateTime = registerDateTime;
+	}
+	
+	void setId(Long id) {
+		this.id = id;
+	}
+	 
+	public Long getId() {
+		return id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	//To do: password -> hashed password
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public LocalDateTime getRegisterDateTime() {
+		return registerDateTime;
+	}
+	
+	//To do: add change password function
+	
+	
+}
